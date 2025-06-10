cd48a4d1640196009e1c70d3be11a5e136147c61
likanug <likanug.g@qq.com>
2025-06-10 14:02:36 +0800

    添加agileboot数据库的创建sql

---
+++ sql/mysql8/01_database.sql
@@ -0,0 +1,5 @@
+-- ----------------------------
+-- 创建agileboot数据库
+-- ----------------------------
+drop database if exists `agileboot`;
+create database `agileboot` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
